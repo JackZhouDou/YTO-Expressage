366a1917ad38cc1b3eb28e3fb0b925a35acda2fa
dlios <15242539014@163.com>
2015-07-02 10:28:38 +0800

    Initial Commit

---
+++ YTO Expressage/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  YTO Expressage
+//
+//  Created by dlios on 15-7-2.
+//  Copyright (c) 2015年 douDou.office. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
